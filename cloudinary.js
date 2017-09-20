5b5ef9e470026d6f00a1952fc0dad419c37f1bde
Alexander Vincent <AlexV9582@yahoo.com>
2017-09-19 21:24:21 -0400

    updating pic size

--- cloudinary.js
+++ cloudinary.js
@@ -11,7 +11,7 @@ cloudinary.config({
 module.exports = function upload(avatar, cb) {
     cloudinary.v2.uploader.upload(avatar, {transformation: [
         {width: 400, height: 400, gravity: "face", radius: "max", crop: "crop"},
-        {width: 200, crop: "scale"}
+        {width: 400, crop: "scale"}
         ] }, 
         function(error, image ) {
             return cb(image);
