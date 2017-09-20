b5f3579a2017695e3b8a8c6a9567b6e9c6008b78
Alexander Vincent <AlexV9582@yahoo.com>
2017-09-19 20:41:39 -0400

    updated variables

--- cloudinary.js
+++ cloudinary.js
@@ -1,4 +1,5 @@
 var cloudinary = require("cloudinary");
+var phone      = require("routes/phoneRoutes")
 cloudinary.config({ 
   cloud_name: 'avincent', 
   api_key: '779359958168278', 
@@ -7,13 +8,15 @@ cloudinary.config({
 
 // Upload code
 
-function upload(userImage) {
-    cloudinary.v2.uploader.upload('../../../testpic.jpg', {transformation: [
+function upload(avatar) {
+    cloudinary.v2.uploader.upload(avatar, {transformation: [
         {width: 400, height: 400, gravity: "face", radius: "max", crop: "crop"},
         {width: 200, crop: "scale"}
         ] }, 
         function(error, image) {
-            console.log(image);
+            return image.url;
         })      
 };
 upload();
+
+module.exports = upload
